b16eef3c99e8ecf2d2803f385ddefcb819a403fc
Irfanm22 <irfan.mahmuti@students.fhnw.ch>
2025-11-02 20:35:58 +0100

    Aggiunto file main.py con struttura base

---
+++ main.py
@@ -0,0 +1,40 @@
+# -------------------------------------------------
+# Projektname: GradeCalc - Interaktiver Notenrechner für Schüler*innen und Student*innen
+# Beschreibung:
+#  GradeCalc ist eine interaktive Python-Anwendung zur Berechnung von Noten und 
+#  Durchschnittswerten. Nutzer*innen können individuelle Noten eingeben, Gewichtungen
+#  festlegen und automatisch den Gesamtdurchschnitt oder benötigte Punkte für 
+#  eine Zielnote berechnen. Die Anwendung speichert Daten in Dateien, kann bestehende
+#  Datensätze laden und unterstützt verschiedene Notenskalen (CH, DE, USA).
+#
+# Autoren : Tim Schlatter, Josselyn Cabrera, Irfan Mahmuti
+# Version: 1.0
+# Datum : 02.11.2025
+#
+# Funktionen
+# - Notenrechner für Schüler*innen (Skala 1.0–6.0) 
+# - Notenrechner für Student*innen (ECTS/Punktesystem) 
+# - Noten und Module eingeben, anzeigen, löschen 
+# - Durchschnitt berechnen (modulbasiert oder gesamt) 
+# - Umrechnung in andere Notenskalen (z. B. CH → DE, USA) 
+# - Prüfungsrechner: Berechnung der benötigten Punkte für eine Ziel 
+# - Note 
+# - Daten speichern und laden (. JSON .TXT-Datei) 
+# - Programm beenden 
+#
+# Abhängigkeiten:
+# - Python 3.10 oder höher
+
+
+
+
+def main():
+    pass
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
